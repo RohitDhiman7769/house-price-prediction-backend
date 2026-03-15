315df19d6ec4d150dbed2b54bcd0558e712fa216
Rohit Dhiman <rohitdhiman265498@gmail.com>
2026-03-15 14:47:55 +0530

    add frontend url

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://house-price-prediction-frontend.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
